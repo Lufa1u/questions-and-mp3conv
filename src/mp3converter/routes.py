4e8208a9337f105f222fc4fdfeadf88e5016a05a
Lufa1u <fortyfack76@gmail.com>
2023-05-21 20:23:43 +0300

    add audio init

--- src/mp3converter/routes.py
+++ src/mp3converter/routes.py
@@ -1,9 +1,10 @@
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, UploadFile, HTTPException
 from uuid import uuid4
 from config import get_db
 from src.mp3converter import manager
 from sqlalchemy.orm import Session
 from src.mp3converter.schemas import UserSchema
+from os import path
 
 
 router = APIRouter()
@@ -13,3 +14,10 @@ router = APIRouter()
 async def create_user(username: str, db: Session = Depends(get_db)):
     user_uuid = str(uuid4())
     return await manager.create_user(username=username, user_uuid=user_uuid, db=db)
+
+
+@router.post(path="/add_audio", response_model=str)
+async def add_audio(user_id: int, user_uuid: str, file: UploadFile):
+    if path.splitext(file.filename)[1] != ".wav":
+        raise HTTPException(status_code=422, detail="Bad file extension.")
+    pass
