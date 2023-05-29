f3b4977718a88442edfc3ab992b312cf12a84435
Lufa1u <fortyfack76@gmail.com>
2023-05-29 23:12:41 +0300

    .env fix

--- config.py
+++ config.py
@@ -10,11 +10,11 @@ path_to_folder = os.environ.get("PATH_TO_FOLDER")
 
 
 class DBConfig:
-    DB_USER = os.environ.get("DB_USER")
-    DB_PASS = os.environ.get("DB_PASS")
-    DB_HOST = os.environ.get("DB_HOST")
-    DB_PORT = os.environ.get("DB_PORT")
-    DB_NAME = os.environ.get("DB_NAME")
+    DB_USER = os.environ.get("POSTGRES_USER")
+    DB_PASS = os.environ.get("POSTGRES_PASSWORD")
+    DB_HOST = os.environ.get("POSTGRES_HOST")
+    DB_PORT = os.environ.get("POSTGRES_PORT")
+    DB_NAME = os.environ.get("POSTGRES_DB")
 
 
 def get_db():
